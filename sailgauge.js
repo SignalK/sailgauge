947c4728976caf53bb02664490525b0995f02a2b
Teppo Kurki <teppo.kurki@iki.fi>
2016-01-13 20:49:58 +0200

    Remove ref to unused ws

--- sailgauge.js
+++ sailgauge.js
@@ -4,6 +4,4 @@ d3 = require('d3');
 SailGauge = require('./lib/sailgauge.js');
 require('./lib/baconslidingtimewindow.js');
 navi = require('./lib/naviutils.js');
-WebSocket = require('ws');
 SignalK = require('signalk-client');
-

8f86db69658fa557f08d34c5dfe075cded04394f
Teppo Kurki <teppo.kurki@iki.fi>
2017-08-19 23:02:36 +0300

    fix(version): use @signalk packages

--- sailgauge.js
+++ sailgauge.js
@@ -1,4 +1,4 @@
 window.SailGauge = require("./lib/sailgauge.js");
 window.d3 = require("d3");
 window.navi = require('./lib/naviutils.js');
-window.SignalK = require('signalk-client');
+window.SignalK = require('@signalk/client');
